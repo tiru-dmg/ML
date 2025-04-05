ce6f3f40cf3e3c3c88dd5b56b333babb1ff32885
Gopi Tirumala <141766397+tiru-dmg@users.noreply.github.com>
2025-04-05 09:57:35 +0530

    Create ML6.py

---
+++ ML6.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+data = {'Color': ['Red', 'Green', 'Blue', 'Red', 'Green']}
+df = pd.DataFrame(data)
+
+print("Original Data:")
+print(df)
+
+encoder = OneHotEncoder(sparse_output=False)
+encoded_array = encoder.fit_transform(df[['Color']])
+encoded_df = pd.DataFrame(encoded_array, columns=encoder.get_feature_names_out(['Color']))
+
+print("\nOne-Hot Encoded Data:")
+print(encoded_df)
+
+final_df = pd.concat([df, encoded_df], axis=1)
+print("\nFinal DataFrame with One-Hot Encoding:")
+print(final_df)
