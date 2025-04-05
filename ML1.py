90bb16003fc1fde0b9bdf9d59437c6803889c5fb
Gopi Tirumala <141766397+tiru-dmg@users.noreply.github.com>
2025-04-05 10:26:16 +0530

    Update ML1.py

--- ML1.py
+++ ML1.py
@@ -1,3 +1,8 @@
+'''Experiment-1:
+Implement and demonstrate the FIND-S algorithm for finding the
+most specific hypothesis based on a given set of training data
+samples. Read the training data from a .CSV file.'''
+
 import csv
 
 def find_s(data):
