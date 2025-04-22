04f23eee3ae3ac50903a3564595efb64cb16b1c8
Gopi Tirumala <141766397+tiru-dmg@users.noreply.github.com>
2025-04-22 17:45:56 +0530

    Create create and compile model.py

---
+++ create and compile model.py
@@ -0,0 +1,17 @@
+from tensorflow.keras import Sequential, Input
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import plot_model
+import numpy as np
+
+model = Sequential([Input(shape=(2,)), Dense(4, activation='relu'), Dense(1, activation='sigmoid')])
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+model.summary()
+
+x = np.array([[0,0],[0,1],[1,0],[1,1]])
+y = np.array([0,1,1,0])
+model.fit(x, y, epochs=10)
+
+test = np.array([[0,0],[0,1],[1,1],[1,1]])
+print(model.predict(test))
+
+plot_model(model, show_shapes=True, show_layer_names=True)
